76a3862d604007098ea47221810d34c917a64638
Vlad Savin <vladislavsavi@gmail.com>
2021-11-14 18:23:45 +0600

    Added craco-alias
    configured aliases
    -@assets
    -@components
    -@root

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoLessPlugin = require('craco-less');
+const CracoAlias = require("craco-alias");
 
 module.exports = {
     plugins: [
@@ -12,5 +13,13 @@ module.exports = {
                 },
             },
         },
+        {
+            plugin: CracoAlias,
+            options: {
+                source: "tsconfig",
+                baseUrl: "./src",
+                tsConfigPath: "./tsconfig.extend.json"
+            }
+        }
     ],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Header} from './components';
+import {Header} from '@components';
 
 function App() {
   return (
